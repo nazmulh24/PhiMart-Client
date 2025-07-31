14d83cb8ce545778e55b7049997668506e12d8c0
Nazmul Hossain <snazmulhossains24@gmail.com>
2025-07-31 22:48:52 +0600

    Tailwind, Router, DaisyUI setup done...

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+// import { BrowserRouter, Routes, Route } from "react-router";
 import "./index.css";
-import App from "./App.jsx";
+import AppRoutes from "./routes/AppRoutes";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   </StrictMode>
 );
