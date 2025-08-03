42baa913c02bdd082dea4d3ab3d8600a1c42882f
Nazmul Hossain <snazmulhossains24@gmail.com>
2025-08-03 22:52:39 +0600

    31.10 Making Carousel Responsive

--- src/components/Carousel/CarouselSlide.jsx
+++ src/components/Carousel/CarouselSlide.jsx
@@ -3,22 +3,24 @@ import bg_image from "../../assets/images/banner-image-bg.jpg";
 const CarouselSlide = ({ title, subtitle, image }) => {
   return (
     <section
-      className="w-full h-[650px] bg-cover bg-center flex justify-center items-center px-8"
+      className="w-full h-[650px] bg-cover bg-center flex justify-center items-center px-4 md:px-8"
       style={{ backgroundImage: `url(${bg_image})` }}
     >
-      <div className="max-w-6xl flex justify-between items-center px-8">
+      <div className="max-w-6xl w-full flex flex-col md:flex-row justify-between items-center px-4 md:px-8">
         {/* left */}
-        <div className="w-1/2 space-y-4">
-          <h1 className="text-5xl font-bold text-gray-900">{title}</h1>
+        <div className="w-full md:w-1/2 space-y-4 text-center md:text-left">
+          <h1 className="text-3xl md:text-5xl font-bold text-gray-900">
+            {title}
+          </h1>
           <p className="text-gray-600">{subtitle}</p>
           <button className="btn btn-secondary px-6 py-3 rounded-full shadow-md">
             Shop product
           </button>
         </div>
         {/* right */}
-        <div className="w-1/2 flex justify-center">
+        <div className="w-full md:w-1/2 flex justify-center">
           <img
-            className="max-w-md drop-shadow-lg"
+            className="max-w-full md:max-w-md drop-shadow-lg"
             src={image}
             alt="Image of a book"
           />
