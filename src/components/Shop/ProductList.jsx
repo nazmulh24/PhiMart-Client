94438f51bf6859092e45885492c76bcfe5b87775
Nazmul Hossain <snazmulhossains24@gmail.com>
2025-08-06 00:29:05 +0600

    33.6 Dynamically Change Price Range

--- src/components/Shop/ProductList.jsx
+++ src/components/Shop/ProductList.jsx
@@ -9,7 +9,7 @@ const ProductList = ({ products, loading }) => {
     );
 
   return (
-    <div className="container grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mx-auto px-4 py-6">
+    <div className=" grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mx-auto px-4 py-6">
       {products.map((product) => (
         <ProductItem key={product.id} product={product} />
       ))}
