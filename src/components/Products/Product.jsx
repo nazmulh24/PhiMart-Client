343f79a39755a8d270598b96844426bb11041828
Nazmul Hossain <snazmulhossains24@gmail.com>
2025-08-05 15:08:05 +0600

    32.6 Creating a Category Section

--- src/components/Products/Product.jsx
+++ src/components/Products/Product.jsx
@@ -24,67 +24,72 @@ const Product = () => {
   }, []);
 
   return (
-    <section className="mx-auto py-16 bg-gray-50">
-      <div className="flex items-center justify-between px-4 md:px-8">
-        <h2 className="text-3xl md:text-4xl font-semibold">
-          Trending Products
-        </h2>
-        <a href="#" className="btn btn-secondary px-6 rounded-full text-lg">
-          See All
-        </a>
-      </div>
-
-      {isloading && (
-        <div className="flex items-center justify-center pt-8">
-          <span className="loading loading-dots loading-xl text-secondary"></span>
+    <section className=" bg-gray-50">
+      <div className="py-8 px-4 max-w-7xl mx-auto">
+        <div className="flex justify-between items-center mb-8">
+          <h2 className="text-3xl font-bold">Trending Products</h2>
+          <a
+            href="#"
+            className="btn btn-secondary px-6 py-4 rounded-full text-lg"
+          >
+            See All
+          </a>
         </div>
-      )}
 
-      {error && (
-        <div className="text-red-500 text-center pt-8 px-3 max-w-full sm:max-w-1/2 mx-auto  flex items-center justify-center">
-          <div role="alert" className="alert alert-error">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 shrink-0 stroke-current"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
-            <span>Error fetching products: {error}</span>
+        {isloading && (
+          <div className="flex items-center justify-center pt-8">
+            <span className="loading loading-dots loading-xl text-secondary"></span>
           </div>
-        </div>
-      )}
+        )}
 
-      {!isloading && !error && products.length > 0 && (
-        <Swiper
-          modules={[Navigation]}
-          spaceBetween={10}
-          slidesPerView={1}
-          breakpoints={{
-            640: { slidesPerView: 2 },
-            1024: { slidesPerView: 3 },
-          }}
-          navigation={true}
-          className="mt-4 px-4 md:px-8 container mx-auto"
-        >
-          {products.map((product) => (
-            <SwiperSlide key={product.id} className="swiper-slide">
-              <ProductItem product={product} />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      )}
-      {products.length === 0 && !isloading && !error && (
-        <div className="text-center py-8 px-4 md:px-8">
-          <p className="text-gray-500">No products available at the moment.</p>
-        </div>
-      )}
+        {error && (
+          <div className="text-red-500 text-center pt-8 px-3 max-w-full sm:max-w-1/2 mx-auto  flex items-center justify-center">
+            <div role="alert" className="alert alert-error">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 shrink-0 stroke-current"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+              <span>Error fetching products: {error}</span>
+            </div>
+          </div>
+        )}
+
+        {!isloading && !error && products.length > 0 && (
+          <Swiper
+            modules={[Navigation]}
+            spaceBetween={10}
+            slidesPerView={1}
+            breakpoints={{
+              640: { slidesPerView: 2 },
+              1024: { slidesPerView: 3 },
+            }}
+            navigation={true}
+            className="mt-4 px-4 md:px-8 container mx-auto"
+          >
+            {products.map((product) => (
+              <SwiperSlide key={product.id} className="swiper-slide">
+                <ProductItem product={product} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        )}
+        {products.length === 0 && !isloading && !error && (
+          <div className="text-center py-8 px-4 md:px-8">
+            <p className="text-gray-500">
+              No products available at the moment.
+            </p>
+          </div>
+        )}
+      </div>
     </section>
   );
 };
