feb56819c8d2c364bed6a93d76b4d3427190fed6
Nazmul Hossain <snazmulhossains24@gmail.com>
2025-08-08 15:41:45 +0600

    35.1 Designing Dashboard Page

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -21,16 +21,16 @@ const AppRoutes = () => {
         <Route path="register" element={<Register />} />
 
         <Route path="activate/:uid/:token" element={<ActivateAccount />} />
-
-        <Route
-          path="dashboard"
-          element={
-            <PrivateRoute>
-              <Dashboard />
-            </PrivateRoute>
-          }
-        />
       </Route>
+
+      <Route
+        path="dashboard"
+        element={
+          <PrivateRoute>
+            <Dashboard />
+          </PrivateRoute>
+        }
+      />
     </Routes>
   );
 };
