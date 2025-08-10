9c1fe9d01ddddfdd5578ca13e03529c75035675d
Nazmul Hossain <snazmulhossains24@gmail.com>
2025-08-10 17:48:24 +0600

    37.2 Implementing Order Page

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -14,6 +14,7 @@ import ForgotPassword from "../pages/ForgotPassword";
 import ResetPassword from "../pages/ResetPassword";
 import ProductDetail from "../pages/ProductDetail";
 import Cart from "../pages/Cart";
+import Orders from "../pages/Orders";
 
 const AppRoutes = () => {
   return (
@@ -48,6 +49,7 @@ const AppRoutes = () => {
         <Route index element={<Dashboard />} />
         <Route path="profile" element={<Profile />} />
         <Route path="cart" element={<Cart />} />
+        <Route path="orders" element={<Orders />} />
       </Route>
     </Routes>
   );
